4cd018ab0a5edec21a17fe925c41cd37ffa1ccf6
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-04-14 02:00:03 +0100

    Refactor code

--- frontend/src/components/BookCard.jsx
+++ frontend/src/components/BookCard.jsx
@@ -22,49 +22,53 @@ const BookCard = ({ book }) => {
   const [showModal, setShowModal] = useState(false);
   return (
     <div>
-        {showModal && <BookModal book={book} showModal={showModal} onClose={() => setShowModal(false)} />}
-        <Card maxW="sm">
-      <CardBody>
-        <Image
-          src={book.imageUrl}
-          className="w-full h-64 object-cover"
-          alt={`Cover image of the book ${book.title}`}
-          borderRadius="lg"
+      {showModal && (
+        <BookModal
+          book={book}
+          showModal={showModal}
+          onClose={() => setShowModal(false)}
         />
-        <Stack mt="6" spacing="3">
-          <Heading size="md">{book.title}</Heading>
-          <Text>{book.author}</Text>
-          <Text>
-            {book.publishYear}
-          </Text>
-          <Text color="blue.600" fontSize="2xl">
-            ${book.price}
-          </Text>
-        </Stack>
-      </CardBody>
-      <Divider />
-      <CardFooter>
-        <div className="flex  items-center justify-around w-full">
-          <FontAwesomeIcon
-            icon={faCircleInfo}
-            className="text-sky-600 cursor-pointer"
-            onClick={() => setShowModal(true)}
+      )}
+      <Card maxW="sm">
+        <CardBody>
+          <Image
+            src={book.imageUrl}
+            className="w-full h-64 object-cover"
+            alt={`Cover image of the book ${book.title}`}
+            borderRadius="lg"
           />
-          <Link to={`/books/edit/${book._id}`}>
+          <Stack mt="6" spacing="3">
+            <Heading size="md">{book.title}</Heading>
+            <Text>{book.author}</Text>
+            <Text>{book.publishYear}</Text>
+            <Text color="blue.600" fontSize="2xl">
+              ${book.price}
+            </Text>
+          </Stack>
+        </CardBody>
+        <Divider />
+        <CardFooter>
+          <div className="flex  items-center justify-around w-full">
             <FontAwesomeIcon
-              icon={faPenToSquare}
-              className="text-yellow-600 cursor-pointer"
+              icon={faCircleInfo}
+              className="text-sky-600 cursor-pointer"
+              onClick={() => setShowModal(true)}
             />
-          </Link>
-          <Link to={`/books/delete/${book._id}`}>
-            <FontAwesomeIcon
-              icon={faTrash}
-              className="text-red-600 cursor-pointer"
-            />
-          </Link>
-        </div>
-      </CardFooter>
-    </Card>
+            <Link to={`/books/edit/${book._id}`}>
+              <FontAwesomeIcon
+                icon={faPenToSquare}
+                className="text-yellow-600 cursor-pointer"
+              />
+            </Link>
+            <Link to={`/books/delete/${book._id}`}>
+              <FontAwesomeIcon
+                icon={faTrash}
+                className="text-red-600 cursor-pointer"
+              />
+            </Link>
+          </div>
+        </CardFooter>
+      </Card>
     </div>
   );
 };
